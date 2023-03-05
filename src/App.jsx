e663b95209d091343a139a2b34853c4dd3fb3f7c
vivianequeiroz <vivianedsqueiroz@gmail.com>
2023-03-05 16:16:41 -0300

    style: add sidebar elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import "./global.css";
+import styles from "./App.module.css";
+
 import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+          <Post />
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80"
+        alt="notebook with a vs code open"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/61950209?v=4"
+        />
+        <strong>Viviane Queiroz</strong>
+        <span>Front-end Software Developer</span>
+      </div>
+
+      <footer>
+        <button>
+          Edit profile <PencilLine size={20} />
+        </button>
+      </footer>
+    </aside>
+  );
+}
