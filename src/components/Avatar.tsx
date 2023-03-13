7316cdd714a24ebc8ed05498e67400d2c6120a42
vivianequeiroz <vivianedsqueiroz@gmail.com>
2023-03-13 19:00:36 -0300

    refact: extend AvatarProps and add spread operator to equal properties

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,18 +1,15 @@
-import React from "react";
+import React, { ImgHTMLAttributes } from "react";
 import styles from "./Avatar.module.css";
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
-  alt?: string;
-  src: string;
 }
 
-export function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
+export function Avatar({ hasBorder = true, ...props }: AvatarProps) {
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
-      alt={alt}
+      {...props}
     />
   );
 }
