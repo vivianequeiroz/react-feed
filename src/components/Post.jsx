9912bd275fdf7b308b6d30d22bddeda3211214ac
vivianequeiroz <vivianedsqueiroz@gmail.com>
2023-03-13 17:00:19 -0300

    fix: add key props to lists to avoid unecessary re render

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -55,14 +55,10 @@ export function Post({ author, publishedAt, content, id }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return (
-              <div key={id}>
-                <p>{line.content}</p>
-              </div>
-            );
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             );
@@ -84,7 +80,7 @@ export function Post({ author, publishedAt, content, id }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment content={comment} />;
+          return <Comment key={comment} content={comment} />;
         })}
       </div>
     </article>
